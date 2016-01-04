e24b9e49ea2d96d0a6ebf8088c4f1fa86af7e03e
Andreas Kloeckner <inform@tiker.net>
2016-01-04 15:21:03 -0600

    Some mucking about towards packaging pocl

---
+++ akllvmdev/build.sh
@@ -0,0 +1,33 @@
+mkdir build
+cd build
+if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    # Linux
+    ../configure \
+        --enable-pic \
+        --enable-optimized \
+        --disable-docs \
+        --enable-targets=host \
+        --disable-terminfo \
+        --disable-libedit \
+        --prefix=$PREFIX \
+        --with-python=$SYS_PYTHON
+
+else
+    # OSX needs 10.7 or above with libc++ enabled
+
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    ../configure \
+        --enable-pic \
+        --enable-optimized \
+        --disable-docs \
+        --enable-targets=host \
+        --disable-terminfo \
+        --disable-libedit \
+        --prefix=$PREFIX \
+        --with-python=$SYS_PYTHON \
+        --enable-libcpp=yes
+
+fi
+
+make -j4 REQUIRES_RTTI=1
+make install
---
+++ pocl/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+chmod +x configure
+
+FLAGS="--disable-icd"
+export LLC_HOST_CPU=x86-64
+
+if [ `uname` == Darwin ]; then
+    ./configure --prefix=$PREFIX --disable-shared $FLAGS
+else
+    ./configure --prefix=$PREFIX $FLAGS
+fi
+
+make
+#make check
+make install
